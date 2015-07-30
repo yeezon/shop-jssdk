d9a45373cee8df02082c71aef155a79c9f24d133
SeemTim <imseem@126.com>
2015-07-30 17:03:24 +0800

    支持url相对协议的写法"//"

--- src/uri.js
+++ src/uri.js
@@ -3,7 +3,7 @@ module.exports = function(sPath){
   var sBase = '/api';
   var sVersion = '/v1';
 
-  if(/^https?:\/\//.test(sPath)){
+  if(/^https?:\/\//.test(sPath) || /^\/\//.test(sPath)){
   	return sPath;
   }else{
   	return sBase + sVersion + '/' + sPath;
