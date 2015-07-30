ccb871238fe7cc9501fa7f00ba25598509ccf319
SeemTim <imseem@126.com>
2015-07-30 17:06:25 +0800

    完成 手机号码/用户名/email注册 通过手机号码重设密码 等接口

--- src/account.js
+++ src/account.js
@@ -5,17 +5,17 @@ var expo = require('./expo.js');
 var aConfig = {
   current: {},
   login: {method: 'POST'},
-  // checkRegistMobile: {method: 'POST', url: 'check_regist_mobile'},
-  // checkResetMobile: {method: 'POST', url: 'check_reset_mobile'},
-  // sendResetValidateSms: {method: 'POST', url: 'send_reset_validate_sms'},
-  // registerWithEmail: {method: 'POST', url: 'register_with_email'},
-  // registerWithUsername: {method: 'POST', url: 'register_with_user_name'},
-  // registerWithMobile: {method: 'POST', url: 'register_with_mobile'},
-  // resetPasswordWithMobile: {method: 'POST', url: 'reset_password_with_mobile'},
-  // resetPasswordWithEmail: {method: 'POST', url: 'reset_password_with_email'},
   logout: {method: 'POST'},
   changePassword: {method: 'POST', url: 'change_password'},
-  save: {method: 'POST'}
+  save: {method: 'POST'},
+  registerWithEmail: {method: 'POST', url: 'register_with_email'},
+  registerWithUsername: {method: 'POST', url: 'register_with_user_name'},
+  registerWithMobile: {method: 'POST', url: 'register_with_mobile'},
+  sendRegistValidateSms: {method: 'POST', url: 'send_regist_validate_sms'},
+  sendResetValidateSms: {method: 'POST', url: 'send_reset_validate_sms'},
+  resetPasswordWithMobile: {method: 'POST', url: 'reset_password_with_mobile'},
+  checkRegistMobile: {method: 'POST', url: 'check_regist_mobile'},
+  checkResetMobile: {method: 'POST', url: 'check_reset_mobile'}
 };
 
 var module = base('account', function(factory){
@@ -25,6 +25,22 @@ var module = base('account', function(factory){
 util.forEach(aConfig, function(value, key){
   exports[key] = expo(module, key);
 });
+
+exports.register = function(param, callback){
+  //
+  switch(param.type){
+    case 'email':
+      module.registerWithEmail(param, callback);
+      break;
+    case 'mobile':
+      module.registerWithMobile(param, callback);
+      break;
+    case 'uname':
+      module.registerWithUsername(param, callback);
+      break;
+  }
+  //
+};
 
 
 /**
@@ -100,10 +116,90 @@ util.forEach(aConfig, function(value, key){
  * &&` ^^^customer^^^ 类型：Object<br/>更新后的用户信息 [查看详情](/development/s/5432566de2931e235b000003)
  * ```
  *
+ * ```register
+ * `` param
+ * &` 类型：Object
+ * &&` ^^^type^^^ 类型：String<br/>注册的账号类型，可选 type 包括：
+ * &&&&` uname: 用户名
+ * &&&&` email: 邮箱
+ * &&&&` mobile: 手机号码
+ * &&` ^^^password^^^ 类型：String<br/>密码
+ * &&` ^^^password_again^^^ 类型：String<br/>重复密码
+ * &&` ^^^verify_code^^^ 类型：String 选填<br/>手机验证码，当注册类型为^^^mobile^^^必须提供，使用[sendRegistValidateSms](#sendRegistValidateSms-param-callback)获取
+ * `` callback
+ * &` 类型：Function( 返回对象 )<br/>提交后的回调函数
+ * &&` ^^^code^^^ 类型：Number<br/>200：注册成功<br/>201：注册失败
+ * &&` ^^^message^^^ 类型：String<br/>注册失败原因（当 code 为 201 时）
+ * &&` ^^^customer^^^ 类型：Object<br/>注册成功的用户信息 [查看详情](/development/s/5432566de2931e235b000003)
+ * ```
+ *
+ * ```sendRegistValidateSms
+ * `` param
+ * &` 类型：Object
+ * &&` ^^^mobile^^^ 类型：String<br/>中国大陆手机号码
+ * &&` ^^^captcha_id^^^ 类型：String 选填<br/>验证码图片 id [获取验证码](/development/s/55b66f1d0abc3e746a000002)
+ * &&` ^^^captcha_value^^^ 类型：String 选填<br/>验证码图片中显示的值 [获取验证码](/development/s/55b66f1d0abc3e746a000002)
+ * `` callback
+ * &` 类型：Function( 返回对象 )<br/>更新后的回调函数
+ * &&` ^^^code^^^ 类型：Number<br/>200：发送成功<br/>214：该操作需要验证码<br/>201：发送失败
+ * &&` ^^^message^^^ 类型：String<br/>发送信息失败原因（当 code 为 201 时）
+ * ```
+ *
+ * ```sendResetValidateSms
+ * `` param
+ * &` 类型：Object
+ * &&` ^^^mobile^^^ 类型：String<br/>中国大陆手机号码
+ * &&` ^^^captcha_id^^^ 类型：String 选填<br/>验证码图片 id [获取验证码](/development/s/55b66f1d0abc3e746a000002)
+ * &&` ^^^captcha_value^^^ 类型：String 选填<br/>验证码图片中显示的值 [获取验证码](/development/s/55b66f1d0abc3e746a000002)
+ * `` callback
+ * &` 类型：Function( 返回对象 )<br/>更新后的回调函数
+ * &&` ^^^code^^^ 类型：Number<br/>200：发送成功<br/>214：该操作需要验证码<br/>201：发送失败
+ * &&` ^^^message^^^ 类型：String<br/>发送信息失败原因（当 code 为 201 时）
+ * ```
+ *
+ * ```resetPasswordWithMobile
+ * `` param
+ * &` 类型：Object
+ * &&` ^^^account^^^ 类型：String<br/>需要重设密码的顾客手机号码账号
+ * &&` ^^^password^^^ 类型：String<br/>密码
+ * &&` ^^^password_again^^^ 类型：String<br/>重复密码
+ * &&` ^^^verify_code^^^ 类型：String<br/>手机验证码，使用[sendResetValidateSms](#sendResetValidateSms-param-callback)获取
+ * `` callback
+ * &` 类型：Function( 返回对象 )<br/>更新后的回调函数
+ * &&` ^^^code^^^ 类型：Number<br/>200：发送成功<br/>214：该操作需要验证码<br/>201：发送失败
+ * &&` ^^^message^^^ 类型：String<br/>发送信息失败原因（当 code 为 201 时）
+ * ```
+ *
+ * ```checkRegistMobile
+ * `` param
+ * &` 类型：Object
+ * &&` ^^^mobile^^^ 类型：String<br/>中国大陆手机号码
+ * `` callback
+ * &` 类型：Function( 返回对象 )<br/>请求后的回调函数
+ * &&` ^^^code^^^ 类型：Number<br/>200：手机号码未注册，可以使用<br/>201：已注册或其他失败信息
+ * &&` ^^^message^^^ 类型：String<br/>已注册或其他失败信息（当 code 为 201 时）
+ * ```
+ *
+ * ```checkResetMobile
+ * `` param
+ * &` 类型：Object
+ * &&` ^^^mobile^^^ 类型：String<br/>中国大陆手机号码
+ * `` callback
+ * &` 类型：Function( 返回对象 )<br/>请求后的回调函数
+ * &&` ^^^code^^^ 类型：Number<br/>200：手机号码已注册，可以重置密码<br/>201：手机号码尚未注册或其他失败信息
+ * &&` ^^^message^^^ 类型：String<br/>201：手机号码尚未注册或其他失败信息（当 code 为 201 时）
+ * ```
+ *
  * @param {current} `callback` 获取当前顾客信息
  * @param {login} `user,callback` 顾客登录
  * @param {logout} `callback` 顾客登出 （必须登录）
  * @param {changePassword} `password,callback` 顾客修改密码 （必须登录）
  * @param {save} `notify,callback` 更新顾客信息 （必须登录）
+ * @param {register} `param,callback` 注册顾客账号
+ * @param {sendRegistValidateSms} `param,callback` 顾客注册手机号码账号时，获取手机验证码
+ * @param {sendResetValidateSms} `param,callback` 顾客找回手机号码账号密码时，获取手机验证码
+ * @param {resetPasswordWithMobile} `param,callback` 重设手机号码账号密码
+ * @param {checkRegistMobile} `param,callback` 检测手机是否未注册（多用于顾客注册手机号码账号时检测用）
+ * @param {checkResetMobile} `param,callback` 检测手机是否已注册（用于顾客重设手机号码账号密码时检测用）
  *
  */
