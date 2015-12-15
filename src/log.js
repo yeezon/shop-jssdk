c3945206782e184d8d9683fdefa09b8034fa207c
SeemTim <imseem@126.com>
2015-12-15 21:33:54 +0800

    add log option

--- src/log.js
+++ src/log.js
@@ -1,12 +1,18 @@
 var util = require('./util.js');
 
-module.exports = function(info){
+module.exports = function(info, option){
   if(window.yhsdDebug){
     var sLog = '';
     util.forEach(info, function(value, key){
       var sEachLog = key + ':' + value + '  ';
       sLog = sLog + sEachLog;
     });
-    console.log(sLog);
+    if(console && console.log){
+    	if(option){
+    		console.log('%c' + sLog, option);
+    	}else{
+    		console.log(sLog);
+    	}
+    }
   }
 };
