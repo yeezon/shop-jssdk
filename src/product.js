6aa96363af957024cd926a884dd465db58702b62
Aaron Z <Aaron Z>
2016-07-15 17:54:17 +0800

    doc: 商品排序文档

--- src/product.js
+++ src/product.js
@@ -25,6 +25,7 @@ exports.get = expo(module, 'get');
  * &&` ^^^vendor^^^ 类型：String 选填<br/>指定商品品牌包含的文字
  * &&` ^^^type^^^ 类型：String 选填<br/>指定商品分类包含的文字
  * &&` ^^^in_stock^^^ 类型：Boolean 选填<br/>指定商品库存是否足够（默认值为 false）
+ * &&` ^^^so^^^ 类型：String 选填<br/>指定商品排序规则，<br/>sale_desc - 销量<br/>price_asc - 价格升序<br/>price_desc - 价格降序<br/>date_desc - 上架时间
  * &&` ^^^size^^^ 类型：Number 选填<br/>指定返回每页的数目
  * &&` ^^^page^^^ 类型：Number 选填<br/>指定返回分页页码
  * `` callback
