1494ec04e75979fa02938d00f7054aa2d1135ec7
min <vngmin@gmail.com>
2019-03-01 17:30:29 +0800

    feat: Add shop.trade_invoice & Fix shop.protecting

--- src/shop.js
+++ src/shop.js
@@ -9,6 +9,7 @@ var module = base('shop', function(factory){
   factory.create('protecting', false, 'POST');
   //
   factory.create('admin_session_status', false, 'GET');
+  factory.create('trade_invoice', './trade_invoice/setting', 'GET');
 });
 
 exports.get = function(){
@@ -26,21 +27,31 @@ exports.get = function(){
   }
 };
 
-exports.protecting = expo(module, 'protecting');
+exports.protecting = function(){
+  var args = [];
+  var sPassword = arguments[0];
+  args = arguments;
+  args[0] = {
+    password: sPassword
+  };
+  module.protecting.apply(module, args);
+};
+
 exports.adminSessionStatus = expo(module, 'admin_session_status');
+exports.tradeInvoice = expo(module, 'trade_invoice');
 /**
- * 店铺信息
+ * 网站信息
  *
  * ```get
  * `` handles
  * &` 类型：String<br/>指定需要返回的内容，用','隔开。<br/>当handles参数为空时，返回所有内容。<br/>可选handles包括：
- * &&` ^^^shop^^^ 店铺基础信息。 [查看详情](/development/s/542508ff71ea1e78fe000008)
- * &&` ^^^linklists^^^ 店铺导航数据。 [查看详情](/development/s/5432558971ea1e4f9f000005)
+ * &&` ^^^shop^^^ 网站基础信息。 [查看详情](/development/s/542508ff71ea1e78fe000008)
+ * &&` ^^^linklists^^^ 网站导航数据。 [查看详情](/development/s/5432558971ea1e4f9f000005)
  * &&` ^^^page_title^^^ 当前页面title
  * &&` ^^^page_desc^^^ 当前页面description
  * &&` ^^^is_mobile_agent^^^ 是否移动设备（根据当前User-Agent判断）
  * &&` ^^^current_datetime^^^ 当前时间
- * &&` ^^^customer_level^^^ 店铺顾客等级数据。
+ * &&` ^^^customer_level^^^ 网站顾客等级数据。
  * `` callback
  * &` 类型：Function( 返回对象 )<br/>获取信息后的回调函数
  * &&& ^^^
@@ -60,9 +71,9 @@ exports.adminSessionStatus = expo(module, 'admin_session_status');
  *
  * ```protecting
  * `` password
- * &` 类型：String<br/>店铺保护密码
+ * &` 类型：String<br/>网站保护密码
  * `` callback
- * &` 类型：Function( 返回对象 )<br/>提交店铺保护密码后的回调函数
+ * &` 类型：Function( 返回对象 )<br/>提交网站保护密码后的回调函数
  * &&` ^^^correct^^^ 类型：Boolean<br/>true: 密码正确，将设置名为 protection_password 的Cookie。此时刷新页面即可访问<br/>false: 密码错误
  * &&& &nbsp;
  * &&& ^^^
@@ -88,8 +99,23 @@ exports.adminSessionStatus = expo(module, 'admin_session_status');
  * &&& ^^^
  * ```
  *
- * @param {get} `[handles,]callback` 获取全部或指定店铺信息
- * @param {protecting} `password,callback` 提交店铺保护密码
+ * ```tradeInvoice
+ * `` callback
+ * &` 类型：Function( 返回对象 )<br/>获取信息后的回调函数
+ * &&` ^^^allow_invoice_for_order^^^ 类型：Boolean<br/>true: 允许订单开发票<br/>false: 不允许订单开发票
+ * &&& &nbsp;
+ * &&& ^^^
+ * &&& {
+ * &&&   "code" : 200,
+ * &&&   "message" : "",
+ * &&&   "allow_invoice_for_order" : true
+ * &&& }
+ * &&& ^^^
+ * ```
+ *
+ * @param {get} `[handles,]callback` 获取全部或指定网站信息
+ * @param {protecting} `password,callback` 提交网站保护密码
  * @param {adminSessionStatus} `callback` 查询管理员是否在当前浏览器登录了网站后台
+ * @param {tradeInvoice} `callback` 查询网站是否允许订单开发票
  *
  */
