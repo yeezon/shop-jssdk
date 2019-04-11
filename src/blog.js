fb0f2c875958da3eaaff78436c23ed3e11e9b6b6
min <vngmin@gmail.com>
2019-04-11 15:58:43 +0800

    feat: Add blog.search

--- src/blog.js
+++ src/blog.js
@@ -3,7 +3,8 @@ var expo = require('./expo.js');
 var util = require('./util.js');
 
 var aConfig = {
-  tags : {url: 'tags'}
+  tags : {url: 'tags'},
+  search : {url: 'search'}
 };
 
 var module = base('blog', function(factory){
@@ -59,8 +60,53 @@ util.forEach(aConfig, function(value, key){
  * &&& ^^^
  * ```
  *
+ * ```search
+ * `` config
+ * &` 类型：Object
+ * &&` ^^^terms^^^ 类型：String 必填<br/>指定检索内容
+ * &&` ^^^page^^^ 类型：Number 选填<br/>指定返回分页页码
+ * &&` ^^^size^^^ 类型：Number 选填<br/>指定返回每页的数目
+ * `` callback
+ * &` 类型：Function(返回对象)<br/>请求后返回的回调函数
+ * &&& ^^^
+ * &&& {
+ * &&&     "code" : 200,
+ * &&&     "message" : "",
+ * &&&     "blogs" : [
+ * &&&         {
+ * &&&             "highlight": {
+ * &&&                 "content.pinyin": [
+ * &&&                     "<mark>测试</mark>"
+ * &&&                 ],
+ * &&&                 "content": [
+ * &&&                     "<mark>测试</mark>"
+ * &&&                 ]
+ * &&&             },
+ * &&&             "data": {
+ * &&&                 "page_url": "/blogs/b001169",
+ * &&&                 "video_name": "",
+ * &&&                 "page_desc": "",
+ * &&&                 "title": "",
+ * &&&                 "show_at": "2016/09/28 20:46:25",
+ * &&&                 "music_album": "",
+ * &&&                 "content": "测试",
+ * &&&                 "blog_type": 1,
+ * &&&                 "blog_tags": [],
+ * &&&                 "blog_author_name": "残酷月光",
+ * &&&                 "music_name": "",
+ * &&&                 "content_capture": "测试",
+ * &&&                 "music_artist": ""
+ * &&&             }
+ * &&&         }
+ * &&&     ],
+ * &&&     "total": 1
+ * &&& }
+ * &&& ^^^
+ * ```
+ *
  * @param {get} `[handle,]callback` 获取单个轻博客
  * @param {getPoly1} `config,callback` 获取轻博客列表
  * @param {tags} `callback` 获取轻博客标签列表
+ * @param {search} `callback` 轻博客全文检索
  *
  */
