7882c0c85d6c2e15663e4fd71555480f5f0b6e2f
min <vngmin@gmail.com>
2021-06-09 18:25:27 +0800

    style: weapp

--- src/weapp.js
+++ src/weapp.js
@@ -2,14 +2,6 @@ var base = require('./base.js');
 var expo = require('./expo.js');
 var util = require('./util.js');
 
-// globalThis 暂时不用
-var _global = {};
-try {
-  _global = global;
-} catch (error) {
-  _global = window;
-}
-
 var aConfig = {
   auth: {method: 'GET', url: 'applet/authorize'},
   payment: {method: 'GET', url: 'payment/applet_go_pay'},
