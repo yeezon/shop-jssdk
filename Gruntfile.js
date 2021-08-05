795f353488320abebf615ca54ece91e9be27b5fb
min <vngmin@gmail.com>
2021-08-05 12:36:12 +0800

    style: Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,75 @@
 var version = require('./src/version.js');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    var jssdkSrc = './src/';
-    var jssdkVer = version.get();
-    var jssdkDest = './dist/jssdk-';
-    var jssdkOutput = jssdkDest + jssdkVer + '.js';
+  var jssdkSrc = './src/';
+  var jssdkVer = version.get();
+  var jssdkDest = './dist/jssdk-';
+  var jssdkOutput = jssdkDest + jssdkVer + '.js';
 
-    var oConfigInit = {
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            options: {
-              jshintrc: true,
-            },
-            dist: {
-                src: jssdkSrc + '*.js'
-            }
+  var oConfigInit = {
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        jshintrc: true,
+      },
+      dist: {
+        src: jssdkSrc + '*.js'
+      }
+    },
+    babel: {
+      options: {
+        sourceMap: false,
+        presets: ["@babel/preset-env"],
+      },
+      dist: {
+        files: {
+          [jssdkOutput]: [jssdkSrc + 'main.js']
+        }
+      },
+    },
+    uglify: {
+      dist: {
+        src: jssdkOutput,
+        dest: jssdkDest + jssdkVer + '.min.js'
+      }
+    },
+    browserify: {
+      dev: {
+        files: {
+          [jssdkOutput]: [jssdkSrc + 'main.js']
         },
-        babel: {
-          options: {
-            sourceMap: false,
-            presets: ["@babel/preset-env"],
-          },
-          dist: {
-            files: {
-              [jssdkOutput]: [jssdkSrc + 'main.js']
-            }
-          },
-        },
-        uglify: {
-            dist:{
-              src: jssdkOutput,
-              dest: jssdkDest + jssdkVer + '.min.js'
-            }
-        },
-        browserify: {
-          dev: {
-            files: {
-              [jssdkOutput]: [jssdkSrc + 'main.js']
-            },
-            options: {
-              watch: true,
-              keepAlive: true,
-              browserifyOptions: {
-                'fullPaths': false,
-                'standalone': 'yhsd'
-              }
-            }
-          },
-          dist: {
-            files: {
-              [jssdkOutput]: [jssdkSrc + 'main.js']
-            },
-            options: {
-                browserifyOptions: {
-                  'fullPaths': false,
-                  'standalone': 'yhsd'
-                }
-            }
+        options: {
+          watch: true,
+          keepAlive: true,
+          browserifyOptions: {
+            'fullPaths': false,
+            'standalone': 'yhsd'
           }
         }
-    };
+      },
+      dist: {
+        files: {
+          [jssdkOutput]: [jssdkSrc + 'main.js']
+        },
+        options: {
+          browserifyOptions: {
+            'fullPaths': false,
+            'standalone': 'yhsd'
+          }
+        }
+      }
+    }
+  };
 
-    grunt.initConfig(oConfigInit);
+  grunt.initConfig(oConfigInit);
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-browserify');
 
-    grunt.registerTask('dev', ['browserify:dev']);
-    grunt.registerTask('dist', ['jshint', 'babel', 'browserify:dist', 'uglify']);
+  grunt.registerTask('dev', ['browserify:dev']);
+  grunt.registerTask('dist', ['jshint', 'babel', 'browserify:dist', 'uglify']);
 
 };
